4e23f289779fa9c178b2771b2e66f0eafc6bd4f6
AntonyBil <beletskiy.ant@gmail.com>
2023-06-14 21:19:57 +0300

    Initial commit

---
+++ 30_UnitTesting/Arithmatic.swift
@@ -0,0 +1,29 @@
+//
+//  Arithmatic.swift
+//  30_UnitTesting
+//
+//  Created by apple on 14.06.2023.
+//
+
+import Foundation
+
+
+class Arithmatic {
+    
+    func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    func sub(num1: Int, num2: Int) -> Int {
+        return num1 - num2
+    }
+    
+    func mul(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    func div(num1: Int, num2: Int) -> Int {
+        return num1 / num2
+    }
+    
+}
---
+++ 30_UnitTesting/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  30_UnitTesting
+//
+//  Created by apple on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let math = Arithmatic()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print("Addition of 2 and 2 is: ", math.add(num1: 2, num2: 2))
+     //   print("Subtraction of 2 and 2 is: ", math.sub(num1: 2, num2: 2))
+     //  print("Division of 2 and 2 is: ", math.div(num1: 2, num2: 2))
+        print("Multiplication of 2 and 2 is: ", math.mul(num1: 2, num2: 5))
+        
+    }
+
+
+}
+
